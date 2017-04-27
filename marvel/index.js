1f79c8a648764f61a2c5378c3561c5dce51c9e5f
Unknown <norkasrom@gmail.com>
2017-04-27 15:01:12 -0700

    12

--- marvel/index.js
+++ marvel/index.js
@@ -1,3 +1,12 @@
 const rq   = require('electron-require');
 const main = rq.remote('./main.js');
 const $    = require("jquery");
+
+
+var buscarPersonaje=function({
+	
+})
+//posiciona el cursor en el cuadro de texto
+$("#txtPersonaje").focus();
+//evento del boton buscar
+$("#btnBuscar").On("click",buscarPersonaje)
