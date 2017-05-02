94c2d111110e126ae1c87cb0a75cc1d5bc8d4fea
Unknown <norkasrom@gmail.com>
2017-05-02 13:31:29 -0700

    marvel finish

--- marvel/index.js
+++ marvel/index.js
@@ -1,41 +1,42 @@
-const rq   = require('electron-require');
-const main = rq.remote('./main.js');
-const $    = require("jquery");
+const rq 	= require('electron-require');
+const main 	= rq.remote('./main.js');
+const $ 	= require("jquery");
 
-
-var buscarPersonaje=function(){
+var buscarPersonaje = function(){
 	var personaje = $("#txtPersonaje").val();
-	var url="http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith="
-	var cantidadComics=0;
-	var cadenaComics="";
-
-	if(personaje==""){
-		alert("Ingrese un personaje")
+	var url = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=67788e74df746a1523d8ebb504ee1008&hash=cf5ec9bfa5a156f031a69417cd0e012c&nameStartsWith="
+	var cantidadComics = 0;
+	var cadenaComics = "";
+	if (personaje == "") {
+		alert("Ingrese el personaje");
 		$("#txtPersonaje").focus();
-		return //ya no continua con la funcion
+		return //Ya no continúa con la función
 	}
-	url=url+personaje;
+	url = url+personaje;
 	$.ajax({
-		dataType:"json",
-		url: url, 
+		dataType: "json",
+		url: url,
 		success:function(response){
-			if(response.code==200){//ok.cat
-			$("#imgFoto").show("slow");
-			$("#imgFoto").attr("scr",response.data.results[0].thumbnail.path+"."+
-									 response.data.results[0].thumbnail.extension)
-			$("#txtDescripcion").html("")
-			$("#txtNombre").html(response.data.results[0].name)
-			if(response.data.results[0].description!=""){
-			$("#txtDescripcion").html("Description: </b>"+response.data.results[0].description)
-		}
-			cantidadComics=response.data.results[0].comics.returned;
-			for(var i=0; i<cantidadComics;i++)
-			cadenaComics+=response.data.results[0].comics,items[i].name+"<br>";
+			if (response.code==200){//ok.cat
+				$("#imgFoto").show("slow");
+				$("#imgFoto").attr("src",response.data.results[0].thumbnail.path+"."+
+										 response.data.results[0].thumbnail.extension)
+				$("#txtNombre").html(response.data.results[0].name);
+				$("#txtDescripcion").html("");
+				if(response.data.results[0].description!=""){
+					$("#txtDescripcion").html("<b>Descripción: </b>"+response.data.results[0].description);
+				}
+				cantidadComics=response.data.results[0].comics.returned;
+				for(var i=0;i<cantidadComics;i++){
+					cadenaComics+=response.data.results[0].comics.items[i].name+"<br>";
+				}
+				$("#txtComics").html(cadenaComics);
 			}
-			$("#txtComics").html(cadenaComics);
+		}
 	})
 }
+
 //posiciona el cursor en el cuadro de texto
 $("#txtPersonaje").focus();
-//evento del boton buscar
+//Evento del boton btnBuscar-click
 $("#btnBuscar").on("click",buscarPersonaje);
--- marvel/main.js
+++ marvel/main.js
@@ -1,28 +1,29 @@
-const {app,BrowserWindow} = require('electron')
+// const app = require('electron').app
+// const BrowserWindow = require('electron').BrowserWindow
+const     {app,BrowserWindow} = require('electron')
 //Ruta donde se encuentra nuestro proyecto
 const path = require('path')
 //Ruta pero en formato URL
-const url  = require('url')
+const url = require('url')
 let PantallaPrincipal;
+
 function muestraPantallaPrincipal(){
 	PantallaPrincipal = new BrowserWindow({
 		width:1024,
 		height:768
 	})
 	PantallaPrincipal.on('closed',function(){
-		PantallaPrincipal = null
+		PantallaPrincipal = null;
 	})
 	PantallaPrincipal.loadURL(url.format({
 		pathname: path.join(__dirname,'index.html'),
-		protocol: 'file',
+		protocol:'file',
 		slashes: true
 	}))
-	//Abre la pantalla de Inspección, donde se encuentra
-	//la consola de Chrome dentro de electron.
-	// PantallaPrincipal.webContents.openDevTools();
+	//PantallaPrincipal.WebContents.openDevTools()
 	PantallaPrincipal.show()
 }
 
-//La aplicación ejecuta este evento cuando
-//el archivo main.js se carga en memoria.
+//La aplicacion ejecuta este evento cuando 
+//el archivo main.js se carga en memoria
 app.on('ready',muestraPantallaPrincipal)
